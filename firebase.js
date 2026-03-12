4cc11d71b6325980b0cd718586fe51fa62240833
venkatapavanpadamati-8899 <venkatapavanpadamati1@gmail.com>
2026-03-12 19:44:24 +0530

    Add Firebase configuration to firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEzJ2LOdgNU6SCMLGHqa0C0hsjQVeOYO8",
+  authDomain: "askexpert-b642b.firebaseapp.com",
+  projectId: "askexpert-b642b",
+  storageBucket: "askexpert-b642b.firebasestorage.app",
+  messagingSenderId: "407440869696",
+  appId: "1:407440869696:web:504234dd881a7e875dd2a5",
+  measurementId: "G-Z313X1J24X"
+};
